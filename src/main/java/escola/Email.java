c8cdbb6f92f06253c23c9ff76bd50fa980dd9cc2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-02 23:39:43 -0300

    Feat: implement basic structure of Email

--- src/main/java/escola/Email.java
+++ src/main/java/escola/Email.java
@@ -1,4 +1,19 @@
 package escola;
 
 public class Email {
+
+    private final String endereco;
+
+    public Email(String endereco) {
+        String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$";
+        if (endereco == null || !endereco.matches(emailRegex)) {
+            throw new IllegalArgumentException("E-mail inválido!");
+        }
+
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
