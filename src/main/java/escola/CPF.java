666b4a54d1ec1dd610f3750819bd11f34f935726
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-02 23:43:17 -0300

    Feat: implement basic structure of CPF

--- src/main/java/escola/CPF.java
+++ src/main/java/escola/CPF.java
@@ -1,4 +1,20 @@
 package escola;
 
 public class CPF {
+
+    private final String numero;
+
+    public CPF(String numero) {
+        String cpfRegex = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})\n";
+        if (numero == null || !cpfRegex.matches(cpfRegex)) {
+            throw new IllegalArgumentException("CPF inválido!");
+        }
+
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    
 }
