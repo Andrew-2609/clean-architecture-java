e8141670b474b05248f0fdb226191cbe7fcfed00
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:24:46 -0300

    Test: assert the messages of thrown exceptions of Telefone in TelefoneTest

--- src/test/java/escola/TelefoneTest.java
+++ src/test/java/escola/TelefoneTest.java
@@ -2,8 +2,7 @@ package escola;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TelefoneTest {
 
@@ -15,9 +14,15 @@ public class TelefoneTest {
     @Test
     void naoDeveriaCriarTelefonesComDDDOuNumeroInvalidos() {
         assertThrows(IllegalArgumentException.class, () -> new Telefone(null, null));
+
         assertThrows(IllegalArgumentException.class, () -> new Telefone("", ""));
-        assertThrows(IllegalArgumentException.class, () -> new Telefone("85", ""));
-        assertThrows(IllegalArgumentException.class, () -> new Telefone("", "91234-5678"));
+
+        IllegalArgumentException exceptionCausedByInvalidDDD = assertThrows(IllegalArgumentException.class, () -> new Telefone("", "91234-5678"));
+        IllegalArgumentException exceptionCausedByInvalidNumero = assertThrows(IllegalArgumentException.class, () -> new Telefone("85", ""));
+
+        assertEquals("DDD inválido!", exceptionCausedByInvalidDDD.getMessage());
+
+        assertEquals("Número de telefone inválido!", exceptionCausedByInvalidNumero.getMessage());
     }
 
 }
