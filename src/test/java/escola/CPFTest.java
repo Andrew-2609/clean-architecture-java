6a8c98d8f4aaf2d25126f90f0abfc490b04d42c5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:18:06 -0300

    Test: implement test 'deveriaCriarCPFComNumeroValido' in CPFTest

--- src/test/java/escola/CPFTest.java
+++ src/test/java/escola/CPFTest.java
@@ -2,9 +2,15 @@ package escola;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CPFTest {
+
+    @Test
+    void deveriaCriarCPFComNumeroValido() {
+        assertDoesNotThrow(() -> new CPF("123.456.789-00"));
+    }
 
     @Test
     void naoDeveriaCriarCPFsComNumeroInvalido() {
