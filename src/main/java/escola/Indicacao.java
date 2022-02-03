3636410bfa68f6377bfd328607273605992b0f36
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:32:18 -0300

    Feat: add attribute 'dataIndicacao' in Indicacao, and also its basic methods

--- src/main/java/escola/Indicacao.java
+++ src/main/java/escola/Indicacao.java
@@ -1,13 +1,31 @@
 package escola;
 
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+
 public class Indicacao {
 
-    private Aluno indicante;
-    private Aluno indicado;
+    private final Aluno indicante;
+    private final Aluno indicado;
+    private final LocalDateTime dataIndicacao;
 
     public Indicacao(Aluno indicante, Aluno indicado) {
         this.indicante = indicante;
         this.indicado = indicado;
+        this.dataIndicacao = now();
     }
-    
+
+    public Aluno getIndicante() {
+        return indicante;
+    }
+
+    public Aluno getIndicado() {
+        return indicado;
+    }
+
+    public LocalDateTime getDataIndicacao() {
+        return dataIndicacao;
+    }
+
 }
