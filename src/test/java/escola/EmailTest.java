805493c60390f49003a8dce13edaa74b12df25d1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 07:49:51 -0300

    Test: implement test 'naoDeveriaCriarEmailComEnderecosInvalidos' in EmailTest

--- src/test/java/escola/EmailTest.java
+++ src/test/java/escola/EmailTest.java
@@ -1,4 +1,18 @@
 package escola;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class EmailTest {
+
+    @Test
+    void naoDeveriaCriarEmailComEnderecosInvalidos() {
+        assertThrows(IllegalArgumentException.class, () -> new Email(null));
+
+        assertThrows(IllegalArgumentException.class, () -> new Email(""));
+
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailInvalido"));
+    }
+
 }
