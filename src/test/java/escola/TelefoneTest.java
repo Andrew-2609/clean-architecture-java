005550ade7e5b0df68dfff16e0342623b29289b2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:05:06 -0300

    Test: implement test 'naoDeveriaCriarTelefonesComDDDOuNumeroInvalidos' in TelefoneTest

--- src/test/java/escola/TelefoneTest.java
+++ src/test/java/escola/TelefoneTest.java
@@ -1,4 +1,17 @@
 package escola;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TelefoneTest {
+
+    @Test
+    void naoDeveriaCriarTelefonesComDDDOuNumeroInvalidos() {
+        assertThrows(IllegalArgumentException.class, () -> new Telefone(null, null));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("", ""));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("85", ""));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("", "91234-5678"));
+    }
+
 }
