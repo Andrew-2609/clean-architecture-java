abac2ad8d2959c07c5735219a7592540c0817ac0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:11:15 -0300

    Chore: update cpfRegex

--- src/main/java/escola/CPF.java
+++ src/main/java/escola/CPF.java
@@ -6,7 +6,7 @@ public class CPF {
     private final String numero;
 
     public CPF(String numero) {
-        String cpfRegex = "([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})\n";
+        String cpfRegex = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
         if (numero == null || !cpfRegex.matches(cpfRegex)) {
             throw new IllegalArgumentException("CPF inválido!");
         }
