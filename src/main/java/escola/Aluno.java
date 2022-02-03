81dfa613fb3f63266489e41a3c59b5f59c11d703
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:29:52 -0300

    Feat: implement basic structure of Aluno

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -1,4 +1,37 @@
 package escola;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// ENTITY
 public class Aluno {
+
+    private final String cpf;
+    private final String nome;
+    private final Email email;
+    private final List<Telefone> telefones = new ArrayList<>();
+
+    public Aluno(String cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public List<Telefone> getTelefones() {
+        return Collections.unmodifiableList(telefones);
+    }
+
 }
