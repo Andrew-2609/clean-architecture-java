2d19defbeee12b4d753c2371522c8aa2331cbc9a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 07:54:12 -0300

    Fix: correct emailRegex

--- src/main/java/escola/Email.java
+++ src/main/java/escola/Email.java
@@ -6,7 +6,7 @@ public class Email {
     private final String endereco;
 
     public Email(String endereco) {
-        String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$";
+        String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         if (endereco == null || !endereco.matches(emailRegex)) {
             throw new IllegalArgumentException("E-mail inválido!");
         }
