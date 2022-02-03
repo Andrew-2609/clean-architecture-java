c10b9b88838c73cb497c04ceb72f8e3d59133336
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-03 08:43:09 -0300

    Feat: implement basic methods of FabricaDeAlunos

--- src/main/java/escola/FabricaDeAlunos.java
+++ src/main/java/escola/FabricaDeAlunos.java
@@ -1,4 +1,21 @@
 package escola;
 
 public class FabricaDeAlunos {
+
+    private Aluno aluno;
+
+    public FabricaDeAlunos comCPFNomeEmail(String cpf, String nome, String email) {
+        this.aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+        return this;
+    }
+
+    public FabricaDeAlunos comTelefone(String ddd, String numero) {
+        this.aluno.adicionarTelefone(ddd, numero);
+        return this;
+    }
+
+    public Aluno construir() {
+        return this.aluno;
+    }
+
 }
