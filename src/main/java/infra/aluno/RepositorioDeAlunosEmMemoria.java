37c41cefa5a1fdbd1895dbaee23a2edb3807ce45
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-07 08:45:39 -0300

    Feat: implement RepositorioDeAlunos interface in RepositorioDeAlunosEmMemoria

--- src/main/java/infra/aluno/RepositorioDeAlunosEmMemoria.java
+++ src/main/java/infra/aluno/RepositorioDeAlunosEmMemoria.java
@@ -1,4 +1,26 @@
 package infra.aluno;
 
-public class RepositorioDeAlunosEmMemoria {
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+
+import java.util.List;
+
+public class RepositorioDeAlunosEmMemoria implements RepositorioDeAlunos {
+
+    @Override
+    public void matricular(Aluno aluno) {
+
+    }
+
+    @Override
+    public Aluno buscarPorCPF(CPF cpf) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> listarTodosAlunosMatriculados() {
+        return null;
+    }
+
 }
