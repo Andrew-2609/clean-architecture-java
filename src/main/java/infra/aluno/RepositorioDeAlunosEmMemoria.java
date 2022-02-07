a165d6acef438b2425473eb6de48412cd7e86aaf
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-07 08:49:38 -0300

    Feat: override methods of RepositorioDeAlunosEmMemoria

--- src/main/java/infra/aluno/RepositorioDeAlunosEmMemoria.java
+++ src/main/java/infra/aluno/RepositorioDeAlunosEmMemoria.java
@@ -1,26 +1,34 @@
 package infra.aluno;
 
 import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.AlunoNaoEncontradoPorCPFException;
 import com.ndrewcoding.escola.dominio.aluno.CPF;
 import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioDeAlunosEmMemoria implements RepositorioDeAlunos {
 
+    private final List<Aluno> alunosMatriculados = new ArrayList<>();
+
     @Override
     public void matricular(Aluno aluno) {
-
+        this.alunosMatriculados.add(aluno);
     }
 
     @Override
     public Aluno buscarPorCPF(CPF cpf) {
-        return null;
+        return alunosMatriculados
+                .stream()
+                .filter(aluno -> aluno.getCpf().equals(cpf.getNumero()))
+                .findFirst()
+                .orElseThrow(() -> new AlunoNaoEncontradoPorCPFException(cpf));
     }
 
     @Override
     public List<Aluno> listarTodosAlunosMatriculados() {
-        return null;
+        return this.alunosMatriculados;
     }
 
 }
