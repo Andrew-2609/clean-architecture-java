2c000088b6307ad9da7b94ddbea9a4c89d74ebd2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-07 07:55:38 -0300

    Feat: create basic method signatures of RepositorioDeAlunos

--- src/main/java/com/ndrewcoding/escola/dominio/aluno/RepositorioDeAlunos.java
+++ src/main/java/com/ndrewcoding/escola/dominio/aluno/RepositorioDeAlunos.java
@@ -1,4 +1,13 @@
 package com.ndrewcoding.escola.dominio.aluno;
 
+import java.util.List;
+
 public interface RepositorioDeAlunos {
+
+    void matricular(Aluno aluno);
+
+    Aluno buscarPorCpf(Aluno aluno);
+
+    List<Aluno> listarTodosAlunosMatriculados();
+
 }
