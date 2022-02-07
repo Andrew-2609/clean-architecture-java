a8de11e4c9493e25fa77d29f4ff9a0647deb7c32
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-07 07:58:14 -0300

    Feat: implement RepositorioDeAlunos interface in RepositorioDeAlunosComJDBC and create its constructor with java.sql.Connection

--- src/main/java/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -1,4 +1,32 @@
 package infra.aluno;
 
-public class RepositorioDeAlunosComJDBC {
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
+
+    private final Connection connection;
+
+    public RepositorioDeAlunosComJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void matricular(Aluno aluno) {
+
+    }
+
+    @Override
+    public Aluno buscarPorCpf(Aluno aluno) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> listarTodosAlunosMatriculados() {
+        return null;
+    }
+
 }
