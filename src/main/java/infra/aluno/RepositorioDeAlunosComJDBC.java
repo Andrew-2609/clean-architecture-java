ecfbdcb2f3bceb2b294993ec52996fb6e7231d31
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-07 08:07:09 -0300

    Feat: implement method matricular() of RepositorioDeAlunosComJDBC

--- src/main/java/infra/aluno/RepositorioDeAlunosComJDBC.java
+++ src/main/java/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -2,8 +2,11 @@ package infra.aluno;
 
 import com.ndrewcoding.escola.dominio.aluno.Aluno;
 import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+import com.ndrewcoding.escola.dominio.aluno.Telefone;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
@@ -16,7 +19,25 @@ public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos {
 
     @Override
     public void matricular(Aluno aluno) {
+        String sql = "INSERT INTO alunos VALUES (?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, aluno.getCpf());
+            preparedStatement.setString(2, aluno.getNome());
+            preparedStatement.setString(3, aluno.getEmail());
+            preparedStatement.execute();
 
+            sql = "INSERT INTO telefones VALUES (?, ?)";
+            preparedStatement = connection.prepareStatement(sql);
+            for (Telefone telefone : aluno.getTelefones()) {
+                preparedStatement.setString(1, telefone.getDdd());
+                preparedStatement.setString(2, telefone.getNumero());
+                preparedStatement.execute();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
