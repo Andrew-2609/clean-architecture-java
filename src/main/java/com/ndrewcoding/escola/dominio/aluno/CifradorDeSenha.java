8efc48b1938f48924b9344d60f92322ff0b2c371
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-08 08:31:16 -0300

    Feat: create basic method signatures in CifradorDeSenhas

--- src/main/java/com/ndrewcoding/escola/dominio/aluno/CifradorDeSenha.java
+++ src/main/java/com/ndrewcoding/escola/dominio/aluno/CifradorDeSenha.java
@@ -2,4 +2,8 @@ package com.ndrewcoding.escola.dominio.aluno;
 
 public interface CifradorDeSenha {
 
+    void cifrarSenha(String senha);
+
+    boolean validarSenhaCifrada(String senha, String senhaCifrada);
+
 }
