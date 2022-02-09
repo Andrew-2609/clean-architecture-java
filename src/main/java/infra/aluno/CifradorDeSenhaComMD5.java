5f21720b306226428e33d2f0dde71278c179e46b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 20:53:51 -0300

    Feat: override and implement methods of CifradorDeSenha interface in CifradorDeSenhaComMD5

--- src/main/java/infra/aluno/CifradorDeSenhaComMD5.java
+++ src/main/java/infra/aluno/CifradorDeSenhaComMD5.java
@@ -2,16 +2,31 @@ package infra.aluno;
 
 import com.ndrewcoding.escola.dominio.aluno.CifradorDeSenha;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class CifradorDeSenhaComMD5 implements CifradorDeSenha {
 
     @Override
-    public void cifrarSenha(String senha) {
-
+    public String cifrarSenha(String senha) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(senha.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = messageDigest.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : bytes) {
+                stringBuilder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+            return stringBuilder.toString();
+        } catch (NoSuchAlgorithmException exception) {
+            throw new RuntimeException("Erro ao gerar hash da senha passada.");
+        }
     }
 
     @Override
     public boolean validarSenhaCifrada(String senha, String senhaCifrada) {
-        return false;
+        return cifrarSenha(senha).equals(senhaCifrada);
     }
 
 }
