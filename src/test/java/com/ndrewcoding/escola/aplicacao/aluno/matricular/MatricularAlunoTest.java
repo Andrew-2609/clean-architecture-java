aa3d97535caf48725a2edac3711993feb16b9491
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 22:01:16 -0300

    Test: implement test 'alunoDeveriaSerPersistido' in MatricularAlunoTest

--- src/test/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
+++ src/test/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -1,4 +1,31 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+import com.ndrewcoding.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MatricularAlunoTest {
+
+    @Test
+    void alunoDeveriaSerPersistido() {
+        RepositorioDeAlunos repositorioDeAlunos = new RepositorioDeAlunosEmMemoria();
+
+        MatricularAluno useCase = new MatricularAluno(repositorioDeAlunos);
+
+        MatricularAlunoDTO dto = new MatricularAlunoDTO("123.456.789-00", "Andrew Monteiro", "andrew@email.com");
+
+        useCase.executar(dto);
+
+        Aluno alunoEncontrado = repositorioDeAlunos.buscarPorCPF(new CPF("123.456.789-00"));
+
+        assertNotNull(alunoEncontrado);
+        assertEquals("Andrew Monteiro", alunoEncontrado.getNome());
+        assertEquals("andrew@email.com", alunoEncontrado.getEmail());
+    }
+
 }
