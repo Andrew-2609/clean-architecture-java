5934ad91e205a711d9e4e421394da9b8bfeb5660
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 21:46:49 -0300

    Feat: add basic implementation of MatricularAluno

--- src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,4 +1,19 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+
 public class MatricularAluno {
+
+    private final RepositorioDeAlunos repositorioDeAlunos;
+
+    public MatricularAluno(RepositorioDeAlunos repositorioDeAlunos) {
+        this.repositorioDeAlunos = repositorioDeAlunos;
+    }
+
+    public void matricular(MatricularAlunoDTO matricularAlunoDTO) {
+        Aluno novoAluno = matricularAlunoDTO.criarAluno();
+        repositorioDeAlunos.matricular(novoAluno);
+    }
+
 }
