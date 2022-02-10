33ed34dc716d392d1cf3fd0ef166116d144408d8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 22:09:18 -0300

    Feat: implement method buscarPorCPF() in MatricularAluno

--- src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,6 +1,7 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
 import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.CPF;
 import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
 
 public class MatricularAluno {
@@ -14,6 +15,11 @@ public class MatricularAluno {
     public void executar(MatricularAlunoDTO matricularAlunoDTO) {
         Aluno novoAluno = matricularAlunoDTO.criarAluno();
         repositorioDeAlunos.matricular(novoAluno);
+    }
+
+    public MatricularAlunoDTO buscarPorCPF(CPF cpf) {
+        Aluno aluno = repositorioDeAlunos.buscarPorCPF(cpf);
+        return new MatricularAlunoDTO(aluno);
     }
 
 }
