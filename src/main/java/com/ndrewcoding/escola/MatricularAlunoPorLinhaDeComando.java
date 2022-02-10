d9b264a355f948c76b88840445554192a1baa98f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 21:55:17 -0300

    Feat: adapt MatricularAlunoPorLinhaDeComando's implementation to new logic

--- src/main/java/com/ndrewcoding/escola/MatricularAlunoPorLinhaDeComando.java
+++ src/main/java/com/ndrewcoding/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,25 +1,18 @@
 package com.ndrewcoding.escola;
 
-import com.ndrewcoding.escola.dominio.aluno.Aluno;
-import com.ndrewcoding.escola.dominio.aluno.CPF;
-import com.ndrewcoding.escola.dominio.aluno.Email;
-import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
+import com.ndrewcoding.escola.aplicacao.aluno.matricular.MatricularAluno;
+import com.ndrewcoding.escola.aplicacao.aluno.matricular.MatricularAlunoDTO;
 import com.ndrewcoding.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
 
 public class MatricularAlunoPorLinhaDeComando {
     public static void main(String[] args) {
 
-        CPF cpf = new CPF("123.456.789-00");
+        String cpf = "123.456.789-00";
         String nome = "Andrew Monteiro";
-        Email email = new Email("andrew@email.com");
+        String email = "andrew@email.com";
 
-        Aluno aluno = new Aluno(cpf, nome, email);
-        aluno.adicionarTelefone("85", "93345-2440");
-
-        RepositorioDeAlunos repositorioDeAlunos = new RepositorioDeAlunosEmMemoria();
-        repositorioDeAlunos.matricular(aluno);
-
-        System.out.println(repositorioDeAlunos.listarTodosAlunosMatriculados());
+        MatricularAluno matricularAluno = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
+        matricularAluno.executar(new MatricularAlunoDTO(cpf, nome, email));
 
     }
 }
