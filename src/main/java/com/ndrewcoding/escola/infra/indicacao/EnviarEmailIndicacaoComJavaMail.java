d8341ef11b9543f57c64a170c0a7c0b04234b4fb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 21:01:00 -0300

    Chore: create class EnviarEmailindicacaoComJavaUtil implementing EnviarEmailIndicacao interface

---
+++ src/main/java/com/ndrewcoding/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.escola.infra.indicacao;
+
+import com.ndrewcoding.escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+
+public class EnviarEmailIndicacaoComJavaMail implements EnviarEmailIndicacao {
+
+    @Override
+    public void enviarPara(Aluno indicado) {
+
+    }
+    
+}
