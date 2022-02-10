82f9f326c72df4294ef0a4cddbf2caecd0458a96
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-09 21:44:56 -0300

    Feat: implement basic structure and constructor method of MatricularAlunoDTO

--- src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoDTO.java
+++ src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoDTO.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
 public class MatricularAlunoDTO {
+
+    private final String nomeAluno;
+    private final String cpfAluno;
+    private final String emailAluno;
+
+    public MatricularAlunoDTO(String nomeAluno, String cpfAluno, String emailAluno) {
+        this.nomeAluno = nomeAluno;
+        this.cpfAluno = cpfAluno;
+        this.emailAluno = emailAluno;
+    }
+
 }
